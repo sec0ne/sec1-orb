d64aeb9ce95ba857bdcbd275a3a2952d67bcd4c8
saurabhthatte <saurabh.thatte@sec1.io>
2024-01-19 14:21:26 +0530

    changes for scanning workspace

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # This example uses envsubst to support variable substitution in the string parameter type.
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
-GITHUB_REPOSITORY=$(circleci env subst "${PARAM_GITHUB_REPOSITORY}")
+SEC1_INTEGRATION_NAME=$(circleci env subst "${SEC1_INTEGRATION_NAME}")
+PARAM_SEC1_API_KEY=$(circleci env subst "${PARAM_SEC1_API_KEY}")
 # If for any reason the TO variable is not set, default to "World"
 #echo "Hello ${TO:-World}!"
 echo "hello world sec1 foss security"
 docker pull sec1security/sec1-foss-security:v1
-docker run -e GITHUB_SERVER_URL=https://github.com -e GITHUB_REPOSITORY="${GITHUB_REPOSITORY}" -it --rm sec1security/sec1-foss-security:v1
+docker run -e SEC1_INTEGRATION_NAME="${SEC1_INTEGRATION_NAME}" -e INPUT_APIKEY="${PARAM_SEC1_API_KEY}" -v /app/circleci-workspace:/app/circleci-workspace -it --rm sec1security/sec1-foss-security:v1
