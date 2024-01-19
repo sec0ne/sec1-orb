527a4abb10b7bb88bf9f0a49d0833b13019d5f3d
saurabhthatte <saurabh.thatte@sec1.io>
2024-01-19 15:54:38 +0530

    changes for workspace

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -7,4 +7,5 @@ PARAM_SEC1_API_KEY=$(circleci env subst "${PARAM_SEC1_API_KEY}")
 #echo "Hello ${TO:-World}!"
 echo "hello world sec1 foss security"
 docker pull sec1security/sec1-foss-security:v1
-docker run -e SEC1_INTEGRATION_NAME="${SEC1_INTEGRATION_NAME}" -e INPUT_APIKEY="${PARAM_SEC1_API_KEY}" -v /tmp/circleci-workspace:/tmp/circleci-workspace -it --rm sec1security/sec1-foss-security:v1
+ls -latr ~/project
+docker run -e SEC1_INTEGRATION_NAME="${SEC1_INTEGRATION_NAME}" -e INPUT_APIKEY="${PARAM_SEC1_API_KEY}" -v ~/project:/tmp/circleci-workspace -it --rm sec1security/sec1-foss-security:v1
