847bfbb864272e3577b8d81eb5b529e2f39949ab
saurabhthatte <saurabh.thatte@sec1.io>
2024-01-20 16:12:05 +0530

    changes for threshold

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -3,6 +3,7 @@
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 SEC1_INTEGRATION_NAME=$(circleci env subst "${SEC1_INTEGRATION_NAME}")
 PARAM_SEC1_API_KEY=$(circleci env subst "${PARAM_SEC1_API_KEY}")
+INPUT_SCANTHRESHOLD=$(circleci env subst "${PARAM_SCAN_THRESHOLD}")
 # If for any reason the TO variable is not set, default to "World"
 #echo "Hello ${TO:-World}!"
 echo "Running Sec1 Security"
@@ -12,8 +13,14 @@ chmod 755 /tmp/sec1-cli
 ls -latr /tmp
 echo "Initiating scan in directory: /home/circleci/project"
 cd /home/circleci/project || exit 1
+THRESHOLD_ARG=""
+
+if [ -n "$INPUT_SCANTHRESHOLD" ]; then
+  THRESHOLD_ARG="-th $INPUT_SCANTHRESHOLD"
+fi
+
 ls -latr /home/circleci/project
-/tmp/sec1-cli scan -s cli --apiKey "${PARAM_SEC1_API_KEY}"
+/tmp/sec1-cli scan -s cli --apiKey "${PARAM_SEC1_API_KEY}" $THRESHOLD_ARG
 #docker pull sec1security/sec1-foss-security:v1
 #ls -latr /home/circleci/project
 #docker run -e SEC1_INTEGRATION_NAME="${SEC1_INTEGRATION_NAME}" -e INPUT_APIKEY="${PARAM_SEC1_API_KEY}" -v /home/circleci/project:/app/circleci-workspace -it --rm sec1security/sec1-foss-security:v1
